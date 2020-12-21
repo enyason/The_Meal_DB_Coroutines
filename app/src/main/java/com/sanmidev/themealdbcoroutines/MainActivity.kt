9e43aa069339eb937f756eb2532b7e9c8e2dfe6f
Oluwasanmi <sanmiaderibigbe@outlook.com>
2020-12-21 20:43:45 +0100

    Setup project

---
+++ app/src/main/java/com/sanmidev/themealdbcoroutines/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sanmidev.themealdbcoroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
